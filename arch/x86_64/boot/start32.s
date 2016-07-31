432225b1aa682aba9ca15c8cac4c506a8c6f3743
krycha <krycha@tutanota.com>
2016-07-31 02:44:35 +0200

    Check - multiboot magic number, cpuid instruction

--- arch/x86_64/boot/start32.s
+++ arch/x86_64/boot/start32.s
@@ -4,12 +4,51 @@
 # Loader from 32-bit protected mode to
 # 64-bit long mode
 
+# Errors are reported in 'eax' register
+# 0xdeadbeef -> multiboot checking failed
+# 0xdead1337 -> cpuid instruction is not supported
+# 0xdeadfeed -> extended cpuid instruction is not supported
+
 .section .text
 .code32
 
 .global start32
 start32:
-    movl $0xb8000, %eax
-    movl $0x41414141, (%eax)
+.multiboot:
+    cmp $0x36d76289, %eax
+    je .cpuid
 
+    mov $0xdeadbeef, %eax
+    hlt
+
+.cpuid:
+    pushfl
+
+    pop %eax
+    mov %eax, %ebx
+
+    xor $(1 << 21), %eax
+    push %eax
+
+    popfl
+    pushfl
+
+    pop %eax
+    cmp %eax, %ebx
+    jne .extended_cpuid
+
+    mov $0xdead1337, %eax
+    hlt
+
+.extended_cpuid:
+    mov $0x80000000, %eax
+    cpuid
+
+    cmp $0x80000001, %eax
+    jae .longmode
+
+    mov $0xdeadfeed, %eax
+    hlt
+
+.longmode:
     hlt
